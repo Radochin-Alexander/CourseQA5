8a8918758fef494dcde2d3368bfcf78aababbc99
Radochin Alexander <radochin.alexander@gmail.com>
2023-06-29 20:53:18 +0300

    Homework#2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bababooey!");
+        int a = 7;
+        int b = 9;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
